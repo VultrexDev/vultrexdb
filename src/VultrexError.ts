1934d86a918ed5e9bb0268bb9de65c61eef10f05
Vultrex Development <54949322+vultrex@users.noreply.github.com>
2020-03-17 21:29:37 +0000

    Delete VultrexError.ts

--- src/VultrexError.ts
+++
@@ -1,7 +0,0 @@
-export default class VultrexError extends Error {
-	public constructor(error: string, name: string) {
-		super();
-		this.name = name || "VultrexError";
-		this.message = error;
-	}
-}
