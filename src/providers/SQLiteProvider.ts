806a51f46036b20197714a4b05a9e478d559ce6b
Vultrex Development <54949322+vultrex@users.noreply.github.com>
2020-04-01 15:10:57 +0100

    Add files via upload

--- src/providers/SQLiteProvider.ts
+++ src/providers/SQLiteProvider.ts
@@ -1,50 +1,52 @@
 import sqlite, { Database } from "sqlite";
-import { SQLiteProviderOptions } from "../interfaces/SQLiteProviderOptions";
 import { RowData } from "../interfaces/RowData";
 
-export class SQLiteProvider {
+interface SQLiteProviderOptions {
+	table: string | 'vultrexdb';
+	fileName: string | 'vultrexdb';
+}
 
+export default class SQLiteProvider {
 	private db: Database;
-
-	private tableName: string;
-	private fileName: string;
+	public table: string;
+	public fileName: string;
 	public initialized: boolean = false;
-	
 	public constructor(config: SQLiteProviderOptions) {
-		this.tableName = config.name || "Vultrex";
-		this.fileName = config.fileName || "VultrexDB";
+		this.table = config.table;
+		this.fileName = config.fileName;
 	}
 
 	public async init() {
 		this.db = await sqlite.open(`./${this.fileName}.db`);
-		await this.db.run(`CREATE TABLE IF NOT EXISTS '${this.tableName}' (key TEXT PRIMARY KEY, value TEXT);`);
+		await this.db.run(`CREATE TABLE IF NOT EXISTS '${this.table}' (key TEXT PRIMARY KEY, value TEXT);`);
 		this.initialized = true;
 	}
 
 	public async set(key: string | number, value: any) {
-		this.db.run(`INSERT OR REPLACE INTO '${this.tableName}' (key, value) VALUES(?, ?);`, key, JSON.stringify(value));
+		this.db.run(`INSERT OR REPLACE INTO '${this.table}' (key, value) VALUES(?, ?);`, key, JSON.stringify(value));
 	}
 
 	public async get<T>(key: string | number, defaultValue: any): Promise<T> {
-		const data = await this.db.get(`SELECT * FROM '${this.tableName}' WHERE key = ?;`, key);
+		const data = await this.db.get(`SELECT * FROM '${this.table}' WHERE key = ?;`, key);
 		return data ? JSON.parse(data["value"]) : defaultValue;
 	}
 
-	public async getAll(): Promise<RowData[]> {
-		const data = await this.db.all(`SELECT * FROM '${this.tableName}';`);
+	public async getAll(key: string | number): Promise<RowData[]> {
+		const query = key ? `SELECT * FROM '${this.table}' WHERE key LIKE '%${key}%';` : `SELECT * FROM '${this.table}';`;
+		const data = await this.db.all(query);
 		return data.map(data => ({ key: data["key"], value: JSON.parse(data["value"]) }));
 	}
 
 	public async size(): Promise<any> {
-		const data = await this.db.get(`SELECT count(*) FROM '${this.tableName}';`);
+		const data = await this.db.get(`SELECT count(*) FROM '${this.table}';`);
 		return data["count(*)"];
 	}
 
 	public async delete(key: string | number): Promise<void> {
-		this.db.run(`DELETE FROM '${this.tableName}' WHERE key = ?;`, key);
+		this.db.run(`DELETE FROM '${this.table}' WHERE key = ?;`, key);
 	}
 
 	public async clear(): Promise<void> {
-		this.db.run(`DELETE FROM '${this.tableName}';`);
+		this.db.run(`DELETE FROM '${this.table}';`);
 	}
 }
